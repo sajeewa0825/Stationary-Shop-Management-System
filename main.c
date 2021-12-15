a4cb6928b16a4553249064c82ed28932010d02f3
sajeewa0825 <sajeewa0825@gmail.com>
2021-12-15 12:32:23 +0530

    creating bill

--- main.c
+++ main.c
@@ -6,6 +6,8 @@ void appendItems();
 void numberOfItem();
 void updateItem();
 void deleteItem();
+void Buy();
+void calculaterBill();
 
 struct items{
     char itemName[20];
@@ -14,6 +16,17 @@ struct items{
     int itemNumber;
 };
 
+struct buy{
+    char itemName[20];
+    int quantity;
+    int itemsPrice;
+    int itemNumber;
+};
+
+struct tempbuy{
+    int quantity;
+    int itemNumber;
+};
 
 int main() {
 
@@ -25,6 +38,7 @@ int main() {
     printf("4. number of items \n");
     printf("5. delete items \n");
     printf("6. update items \n");
+    printf("7. buy items \n");
     scanf("%d",&n);
     if(n==0){
         addItems();
@@ -40,6 +54,8 @@ int main() {
         updateItem();
     }else if(n==5){
         deleteItem();
+    }else if(n==7){
+        Buy();
     }
     return 0;
 }
@@ -70,6 +86,7 @@ void addItems(){
     }
 
     fclose(file);
+    main();
 }
 
 void appendItems(){
@@ -98,6 +115,7 @@ void appendItems(){
     }
 
     fclose(file);
+    main();
 }
 
 void readItemdata(){
@@ -113,6 +131,7 @@ void readItemdata(){
         }
     }
     fclose(file);
+    main();
 }
 
 void searchItem(){
@@ -137,6 +156,7 @@ void searchItem(){
         }
     }
     fclose(file);
+    main();
 }
 
 void numberOfItem(){
@@ -151,6 +171,7 @@ void numberOfItem(){
         printf("Record is %d\n",n);
     }
     fclose(file);
+    main();
 }
 
 void updateItem(){
@@ -192,7 +213,7 @@ void updateItem(){
             fclose(file1);
         }
     }
-
+    main();
 }
 
 
@@ -230,6 +251,59 @@ void deleteItem(){
             fclose(file1);
         }
     }
+    main();
+}
 
+void Buy(){
+    FILE *file,*file1;
+    int check=1;
+    struct items s;
+    struct tempbuy tb;
+    file= fopen("itemdata.dat","r");
+    file1= fopen("tempbuy.dat","w");
+    if(file==NULL &&  file1 == NULL){
+        printf("open fail\n");
+    } else{
+        printf("ItemNumber\t\tName\t\tPrice\t\tquantity");
+        while (fread(&s,sizeof (s),1,file)){
+            printf("\n%d\t\t\t%s\t\t\t%d\t\t\t%d",s.itemNumber,s.itemName,s.itemsPrice,s.quantity);
+        }
+        fclose(file);
+        printf("\n=========================\n");
+        do {
+            printf("Enter item number \n");
+            scanf("%d",&tb.itemNumber);
+            printf("Enter item quantity \n");
+            scanf("%d",&tb.quantity);
+            fwrite( &tb,sizeof(tb),1,file1);
+            printf("close enter 0\n"
+                   "contune enter 1  \n");
+            scanf("%d",&check);
+            printf("\n=========================\n");
+        } while (check==1);
+        fclose(file1);
+
+    }
+    calculaterBill();
+    main();
+}
+
+void calculaterBill(){
+    FILE *file,*file1;
+    struct tempbuy tb;
+    file= fopen("itemdata.dat","r");
+    file1= fopen("tempbuy.dat","r");
+    if(file==NULL &&  file1 == NULL){
+        printf("open fail\n");
+    } else{
+        while (fread(&tb,sizeof (tb),1,file1)){
+            printf("\n%d\t\t\t%d",tb.quantity,tb.itemNumber);
+        }
+        printf("\n");
+        fclose(file);
+        fclose(file1);
+
+    }
+    main();
 }
 
